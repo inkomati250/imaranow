7f015ceadf45985b5e7ca28c3a8b929f7bb4a6d5
Ivan Nkomati <47068719+inkomati250@users.noreply.github.com>
2025-07-18 16:47:14 +0000

    Fri, Jul 18, 2025, 6:47 PM +02:00

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
     path('category/<slug:slug>/', views.category_view, name='category_view'),
+    path('subscribe/', views.subscribe, name='subscribe'),
 ]
