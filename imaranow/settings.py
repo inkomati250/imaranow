b792f578a0aa1c926ae1c085756a58b1074fdb6e
Ivan Nkomati <47068719+inkomati250@users.noreply.github.com>
2025-07-18 17:53:58 +0000

    Fri, Jul 18, 2025, 7:53 PM +02:00

--- imaranow/settings.py
+++ imaranow/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'imagekit',
     'tailwind',
     'taggit',
+    'django.contrib.humanize',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
